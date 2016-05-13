cc7560c50400dcd1c5f0fe20794bee069d45b29e
czaks <marcin@6irc.net>
2016-05-13 13:39:13 +0200

    gitref for new url

--- tools/gitref.php
+++ tools/gitref.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo -n '+ <a href="https://github.com/ctrlcctrlv/infinity/">infinity</a> + <a href="https://github.com/czaks/8chan/">8ch.pl</a> ' > .installed
+echo -n '+ <a href="https://github.com/ctrlcctrlv/infinity/">infinity</a> + <a href="https://github.com/vichan-devel/infinity/">8ch.pl</a> ' > .installed
 git describe --long --tags >> .installed
 
 
